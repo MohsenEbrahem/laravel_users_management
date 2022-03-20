577e2094f56bb2e8987f3c8190178324c31e815e
MohsenEbrahem <itmohseneb@gmail.com>
2022-03-20 11:05:31 +0000

    added email verfication

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\AssignRoleController;
 use App\Http\Controllers\DeleteUserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,10 +24,31 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
 Route::resource('/post',PostController::class)->middleware('auth');
 Route::get('/assign-role', [AssignRoleController::class, 'AssignRole'])->middleware('checkadminrole');
 Route::get('/delete-user',[DeleteUserController::class,'DeleteUser'])->middleware('checkadminrole');
+
+
+
+/*
+the next three routes are required to implement email verification
+*/
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
